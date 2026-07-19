406fb6dd5ea30c0668b026bac1d2de65487092be
Ankit Majumder <ankitmajumder@Ankits-MacBook-Air.local>
2026-07-19 19:52:21 +0530

    Changed navbar logo to profile image

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,15 @@
+import profile from "../assets/profile.png"; 
 export default function Navbar() {
   return (
     <nav className="fixed top-5 left-0 w-full z-50">
       <div className="max-w-6xl mx-auto px-8">
         <div className="h-16 rounded-full bg-black/40 backdrop-blur-2xl border border-white/10 flex items-center justify-between px-8 shadow-2xl">
 
-        <h1 className="text-lg font-black tracking-wide">
-          AM
-        </h1>
+        <img
+         src={profile}
+         alt="Ankit"
+         className="w-10 h-10 rounded-full object-cover"
+        />
 
         <div className="hidden md:flex gap-10 text-sm uppercase tracking-widest">
           <a href="#home"
